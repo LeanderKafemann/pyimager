e179369fafecdd33fc6cb8d758da59f546f6cbae
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2024-02-05 12:52:34 +0100

    update to v2.0.0

---
+++ src/pyimager/README.txt
@@ -0,0 +1,4 @@
+pyimager wurde von Leander Kafemann programmiert.
+Trotzdem wird er keine Haftung übernehmen und ist für jegliche Schäden nicht verantwortlich.
+Bei auftretenden Fehlern können Sie ihn aber gern kontaktieren,
+seine Adresse erhalten Sie über die Funktion about.
--- src/pyimager/__init__.py
+++ src/pyimager/__init__.py
@@ -3,13 +3,15 @@ PYIMAGER
 funcs:
 display -- displays image
 compress -- compresses image file
+decompress -- decompresses image file
+compressor -- compresses whole file anew
 about -- returns information about your release
 """
 def about():
     """
     Returns information about your release and other projects by LK
     """
-    return {"Version":(1, 2, 2), "Author":"Leander Kafemann", date:"12.1.2024", recommend:("Büro by LK", "Verschlüsseler by LK", "flappy bird by LK", "naturalsize by LK"), feedbackTo: "leander@kafemann.berlin"}
+    return {"Version":(2, 0, 0), "Author":"Leander Kafemann", date:"5.2.2024", recommend:("Büro by LK", "Verschlüsseler by LK", "flappy bird by LK", "naturalsize by LK"), feedbackTo: "leander@kafemann.berlin"}
 
 import pycols
 c = pycols.color()
@@ -61,3 +63,30 @@ def compress(path: str, target: str = ""):
         target = path
     with open(target, "w", encoding="utf-8") as f:
         f.write(im)
+
+def decompress(path: str, target: str = ""):
+    """
+    Decompresses LKIM with replacing frequent pixels.
+    When target is left empty, the given file is overwritten.
+    """
+    with open(path, "r", encoding="utf-8") as f:
+        im = f.read()
+    for i in el:
+        im = im.replace(f"§{i}§", i*50)
+        im = im.replace(f"&{i}&", i*20)
+        im = im.replace(f"%{i}%", i*10)
+        im = im.replace(f"${i}$", i*5)
+    if target == "":
+        target = path
+    with open(target, "w", encoding="utf-8") as f:
+        f.write(im)
+
+def compressor(path: str, target: str = ""):
+    """
+    Decompresses and then compresses LKIM anew.
+    When target is left empty, the given file is overwritten.
+    """
+    if target == "":
+        target = path
+    decompress(path, target)
+    compress(target, target)
