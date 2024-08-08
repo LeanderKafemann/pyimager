70ced645ea84002fcef5da905db6faadb32490f4
Leander Kafemann <151378359+LeanderKafemann@users.noreply.github.com>
2024-08-08 13:17:01 +0200

    update test file
    -add includeComb6 statement

--- test/compressor.py
+++ test/compressor.py
@@ -3,7 +3,7 @@ import time, os
 path = "./"+input("Namen eingeben: ")+".lkim"
 tgt = "./"+input("Zieldatei angeben: ")+".lkim"
 print(os.stat(path).st_size)
-pi.compressor(path, tgt)
+pi.compressor(path, tgt, True)
 pi.display(tgt)
 print(os.stat(tgt).st_size)
 time.sleep(3)
